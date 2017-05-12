412674d2948525eff6f6a770f206b20d1c866447
Dmitry Kabanov <dkabanov6@gmail.com>
2017-05-12 07:53:01 +0500

    Правки замечаний

--- js/init-map-min.js
+++
@@ -1,1 +0,0 @@
-"use strict";function initMap(){var a={lat:55.6871,lng:37.5295859},b=new google.maps.Map(document.getElementById("map"),{center:a,scrollwheel:!1,zoom:18});new google.maps.Marker({map:b,position:a})}
--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,35 @@
 'use strict';
 
+function initMap() {
+  var myLatLng = {lat: 55.6871, lng: 37.5295859};
+
+  // Create a map object and specify the DOM element for display.
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: myLatLng,
+    scrollwheel: false,
+    zoom: 18
+  });
+
+  // Create a marker and set its position.
+  var marker = new google.maps.Marker({
+    map: map,
+    position: myLatLng
+  });
+}
+
+function resizeMap() {
+  if (typeof map === "undefined") return;
+
+  setTimeout(function() {
+    resizingMap();
+  }, 400);
+}
+
+function resizingMap() {
+  if (typeof map === "undefined") return;
+  google.maps.event.trigger(map, "resize");
+}
+
 (function () {
   var btnWriteUs = document.querySelector('.btn-write-us');
   var indexInfoMap = document.querySelector('.index-info-map');
@@ -55,4 +85,8 @@
       openModal(evt);
     }
   })
+
+  indexInfoMap.addEventListener('click', function() {
+    resizeMap();
+  });
 })();
